1f990fb223d3700f975d38c29b3652f8a67315f4
Ammar ASLAN <ammaraslan@gmail.com>
2021-12-23 13:48:40 +0300

    Add files via upload

--- PythonRoot_7_ucgen.py
+++ PythonRoot_7_ucgen.py
@@ -1,1 +1,6 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"name":"PythonRoot_7_ucgen.py","provenance":[],"collapsed_sections":[],"authorship_tag":"ABX9TyN7oBBisgsiySifDzwdkkQ6"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","metadata":{"id":"YKGPbaUgF9uV","executionInfo":{"status":"ok","timestamp":1638365546857,"user_tz":-180,"elapsed":4,"user":{"displayName":"Ammar Aslan","photoUrl":"https://lh3.googleusercontent.com/a-/AOh14Gi_efrByO6tItcAHpnFPWJAzEhPyiBBm3-6K3-T8w=s64","userId":"05014449169115974657"}}},"source":["pi=3.14\n","isim=\"Ammar ASLAN\"\n","def AlanHesapla(tabanalanı,yukseklik):\n","  return tabanalanı*yukseklik/2\n","def CevreHesapla(a,b,c):\n","  return a+b+c\n"],"execution_count":3,"outputs":[]}]}
+pi=3.14
+isim="Ammar ASLAN"
+def AlanHesapla(tabanalanı,yukseklik):
+  return tabanalanı*yukseklik/2
+def CevreHesapla(a,b,c):
+  return a+b+c
